1ba91451c5be6fbc5ec15b2ab5d18176d34a92e6
sararagu <sarasrips13@gmail.com>
2022-01-11 16:10:21 +0530

    package changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
+import { OngoingProjectsComponent } from './ongoing-projects/ongoing-projects.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactUsComponent,
+    ServicesComponent,
+    OngoingProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'Home-Page', component: HomePageComponent },
+      { path: 'Contact-Us', component:ContactUsComponent },
+      { path: 'Service', component: ServicesComponent},
+      { path: 'Ongoing-Project', component:OngoingProjectsComponent} 
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
